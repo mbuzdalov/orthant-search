bc73348848988518f917b1841911c8368d5288d6
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-01-14 18:09:11 +0300

    Add a primitive benchmark for non-dominated sorting.

---
+++ benchmarking/src/main/java/ru/ifmo/orthant/nds/Instances.java
@@ -0,0 +1,48 @@
+package ru.ifmo.orthant.nds;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public final class Instances {
+    private Instances() {}
+    private static final int N_INSTANCES = 10;
+
+    public static double[][][] generateUniformHypercube(int n, int d) {
+        Random random = new Random(n * 63142311L + d * 55182243);
+        double[][][] rv = new double[N_INSTANCES][n][d];
+        for (int x = 0; x < N_INSTANCES; ++x) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < d; ++j) {
+                    rv[x][i][j] = random.nextDouble();
+                }
+            }
+        }
+        return rv;
+    }
+
+    public static double[][][] generateUniformHyperplanes(int n, int d, int f) {
+        Random random = new Random(n * 772983252L + d * 52462563);
+        int frontSize = n / f;
+        int firstFrontSize = n - (f - 1) * frontSize;
+        double[][][] rv = new double[N_INSTANCES][n][d];
+        for (int x = 0; x < N_INSTANCES; ++x) {
+            for (int i = 0; i < firstFrontSize; ++i) {
+                double sum = 1.0;
+                for (int j = d - 1; j > 0; --j) {
+                    rv[x][i][j] = sum * (1 - Math.pow(1 - random.nextDouble(), 1.0 / j));
+                    sum -= rv[x][i][j];
+                }
+                rv[x][i][0] = sum;
+            }
+            for (int i = firstFrontSize; i < n; ++i) {
+                System.arraycopy(rv[x][i - frontSize], 0, rv[x][i], 0, d);
+                for (int j = 0; j < d; ++j) {
+                    rv[x][i][j] += 1e-9;
+                }
+            }
+            Collections.shuffle(Arrays.asList(rv), random);
+        }
+        return rv;
+    }
+}
---
+++ benchmarking/src/main/java/ru/ifmo/orthant/nds/JMHBenchmark.java
@@ -0,0 +1,57 @@
+package ru.ifmo.orthant.nds;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import ru.ifmo.orthant.impl.NaiveOrthantSearch;
+import ru.ifmo.orthant.nds.impl.NaiveImplementation;
+import ru.ifmo.orthant.nds.impl.OrthantImplementation;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Timeout(time = 1, timeUnit = TimeUnit.HOURS)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 1, time = 1)
+@Fork(value = 3)
+public class JMHBenchmark {
+    private NonDominatedSorting sorting;
+    private double[][][] instances;
+    private int[] ranks;
+
+    @Param({"10", "100", "1000", "10000"})
+    private int n;
+
+    @Param({"2", "3", "4", "5", "6", "7", "8", "9", "10"})
+    private int d;
+
+    @Param({"uniform.hypercube", "uniform.hyperplanes.f1"})
+    private String datasetId;
+
+    @Param({"NaiveImplementation", "OrthantNaive"})
+    private String algorithmId;
+
+    @Setup
+    public void initialize() {
+        switch (datasetId) {
+            case "uniform.hypercube": instances = Instances.generateUniformHypercube(n, d); break;
+            case "uniform.hyperplanes.f1": instances = Instances.generateUniformHyperplanes(n, d, 1); break;
+            default: throw new AssertionError("Dataset ID '" + datasetId + "' is not known");
+        }
+        switch (algorithmId) {
+            case "NaiveImplementation": sorting = new NaiveImplementation(n, d); break;
+            case "OrthantImplementation": sorting = new OrthantImplementation(new NaiveOrthantSearch(n, d)); break;
+            default: throw new AssertionError("Algorithm ID '" + algorithmId + "' is not known");
+        }
+        ranks = new int[n];
+    }
+
+    @Benchmark
+    public void benchmark(Blackhole bh) {
+        for (double[][] dataset : instances) {
+            sorting.sort(dataset, ranks);
+            bh.consume(ranks);
+        }
+    }
+}
